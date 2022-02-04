1b99ff535cf11d9d8daa37690fca0ac1804589c6
Pranali Vyas <pranali@icreativetechnolabs.com>
2022-02-04 16:50:00 +0530

    [DEL] file removed

--- 42_program.py
+++
@@ -1,9 +0,0 @@
-def iterdict(d):
-  for k,v in d.items():
-     if type(v) ==dict:
-         iterdict(v)
-     else:
-         print (k,":",v)
-
-D1={1: {2: {3: 4, 5: 6}, 3: {4: 5, 6: 7}}, 2: {3: {4: 5}, 4: {6: 7}}}
-iterdict(D1)
