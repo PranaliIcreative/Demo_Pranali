e5e20d3efe3166bb0d472f9d28fe7123d54d0556
Pranali Vyas <pranali@icreativetechnolabs.com>
2022-03-01 18:29:57 +0530

    [IMP]Modified 01-03-2022

--- custom_addons_1/pranali_work/models/models.py
+++ custom_addons_1/pranali_work/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
 from odoo.tools.populate import compute
 
 
@@ -16,6 +17,7 @@ class pranali_work(models.Model):
     age=fields.Integer()
     test_date1 = fields.Date(string='Test Date1')
     test_date2 = fields.Date(string='Test Date2')
+    creation_date = fields.Datetime(string='Creation Date',default=fields.Datetime.now)
     rating_avg = fields.Selection([(' ',' '),
                                    ('poor','Poor'),
                                    ('bad','Bad'),
@@ -23,7 +25,7 @@ class pranali_work(models.Model):
                                    ('good','Good'),
                                    ('very_good','Very Good'),
                                    ('excellent','Excellent')],string="Rating")
-    f_name_id=fields.Many2many('res.partner.category',String='worker name')
+    f_name_id=fields.Many2many('res.partner.category','f_name_table',String='worker name')
     # sale_order_ids = fields.Many2many('sale.order', 'sale_order_transaction_rel', 'transaction_id', 'sale_order_id',
     #                                   string='Sales Orders', copy=False, readonly=True)
     status =fields.Selection([('default','Default'),('registered','Registered'),('in_process','In Process'),('testing','Testing'),('completed','Completed')], default='default')
--- custom_addons_1/smart_view/models/smart.py
+++ custom_addons_1/smart_view/models/smart.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class SmartView(models.Model):
     _name = 'smart.view'
+    _inherit = ['mail.thread','mail.activity.mixin']
+
 
     # _description = 'Smart features practice'
 
-    name = fields.Char()
+    name = fields.Char(tracking=True)
     mobile_number = fields.Char()
+    last_name=fields.Char()
     height = fields.Float()
 
     _sql_constraints = [
@@ -18,21 +21,25 @@ class SmartView(models.Model):
     ]
 
 
-    @api.constrains('name')
+    @api.constrains('name', 'mobile_number')
     def constrains_reconcile(self):
         for record in self:
             if record.name=="Radha" or record.name=="radha" :
                 raise UserError('This user name is blocked!')
-
-    @api.constrains('mobile_number')
-    def constrain_mobileno(self):
-        for record in self:
-            if record.name.isalpha() != True:
+            elif not record.name.isalpha():
                 raise UserError('Name must have only alphabets')
-            if len(record.mobile_number)>10:
-                raise UserError('the digits must be 10')
-            elif len(record.mobile_number)<10:
-                raise UserError('the digits must be 10')
+
+            if record.mobile_number.isalpha():
+                raise ValidationError('Mobile must have only numbers')
+            elif not len(record.mobile_number) == 10:
+                raise ValidationError('the digits must be 10')
+            # elif len(record.mobile_number) < 10:
+            #     raise UserError('the digits must be 10')
+
+    def Conferm(self):
+        print("-------------------------------------->",type(self.id))
+        display_message ="user confermed"
+        self.message_post(body=display_message)
 
 
     def action_testing(self):
